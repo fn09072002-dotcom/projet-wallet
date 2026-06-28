1aca48acde31bef0748e74d10b10b1f20aaf49d2
fatou <fn09072002@gmail.com>
2026-06-28 09:40:17 +0000

    feat: ajout fonctions menu dans controller

--- controller.php
+++ controller.php
@@ -1,1 +1,22 @@
 <?php
+
+
+function afficherMenu() {
+    echo "\n** Menu Distributeur **\n";
+    echo "1 - Créer Wallet\n";
+    echo "2 - Faire Dépôt\n";
+    echo "3 - Faire Retrait\n";
+    echo "4 - Lister les Transactions\n";
+    echo "0 - Quitter\n";
+}
+
+function traiterChoix($choix, &$wallets, &$transactions) {
+    switch ($choix) {
+        case "1": break;
+        case "2": break;
+        case "3": break;
+        case "4": break;
+        case "0": echo "Au revoir !\n"; break;
+        default: echo "Choix invalide, veuillez réessayer\n";
+    }
+}
